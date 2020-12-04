52633cd154b89ab1d1302fbc715bbc5014a28ef9
hikaru <274173368@qq.com>
2020-12-04 14:33:47 +0800

    去除dataEmpty样式

--- src/components/data-empty.vue
+++ src/components/data-empty.vue
@@ -18,20 +18,6 @@ export default {
 };
 </script>
 <style scoped>
-    .v-charts-data-empty {
-        position: absolute;
-        left: 0;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-color: rgba(255, 255, 255, 0.7);
-        color: #888;
-        font-size: 14px;
-        z-index: 999;
-    }
     .f16 {
         padding-top: 30px;
         margin-left: 30px;
